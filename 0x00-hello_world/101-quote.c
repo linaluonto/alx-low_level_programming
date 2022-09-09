b689b20d079f54af9e2c909559e99433cedabc3e
linaluonto <107812991+linaluonto@users.noreply.github.com>
2022-09-09 04:27:59 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	int putchar(int "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	int putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
