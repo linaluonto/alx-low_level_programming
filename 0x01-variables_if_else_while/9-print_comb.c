fafb501135f15c45b97c8d0278022cd3d63357c4
linaluonto <107812991+linaluonto@users.noreply.github.com>
2022-09-12 11:47:22 -0700

    Upload task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- program to print single digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+{
+		putchar (n);
+		if (n != 57)
+		{
+			putchar (44);
+			putchar (32);
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
