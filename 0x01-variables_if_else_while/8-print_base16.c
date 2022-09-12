6c73ed2f8c9841779a0fbe05374b8df888f08bc0
linaluonto <107812991+linaluonto@users.noreply.github.com>
2022-09-12 10:25:34 -0700

    Update task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,11 @@
 int main(void)
 {
 	int n;
+	int ch;
 
 	for (n = 48; n <= 57; n++)
 		putchar (n);
-	for (n = 97; n <= 102; n++)
-		putchar (n);
+	for (ch = 97; ch <= 102; ch++)
+		putchar (ch);
 	putchar ('\n');
 }
