37cea17257c455b643b6fa782e92ad119381b9c7
linaluonto <107812991+linaluonto@users.noreply.github.com>
2022-09-13 13:13:37 -0700

    Update task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- prints lowercase alphabet ten times
+ */
+
+void print_alphabet_x10(void)
+{
+	int n = 1;
+	char ch;
+
+	while (n <= 10)
+	{
+		for (ch = 'a'; 'a' <= 'z'; ch++)
+			_putchar(ch);
+		n++;
+	}
