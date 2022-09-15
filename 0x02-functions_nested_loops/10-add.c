104edcbb1ea7fdbd20c90216b5da63d71c45c002
linaluonto <107812991+linaluonto@users.noreply.github.com>
2022-09-14 19:50:20 -0700

    Upload task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - add two integers and returns the result
+ * @a: first integer to be added
+ * @b: second integer to be added
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
