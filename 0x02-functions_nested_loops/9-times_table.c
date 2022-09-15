51faa4633cbb78426b53c12e07826a5d119263ba
linaluonto <107812991+linaluonto@users.noreply.github.com>
2022-09-14 19:21:58 -0700

    Upload task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int v, h, r;
+
+	for (h = 0; h <= 9; h++)
+	{
+		for (v = 0; v <= 9; v++)
+		{
+			k = v * h;
+
+			if (v == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && v != 0)
+			{
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar(k + '0');
+			}
+		}
+		_putchar(',');
+		_putchar('\n');
+	}
+}
