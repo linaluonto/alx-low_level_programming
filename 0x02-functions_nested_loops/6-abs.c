b0a282d5853bc3a7a0c46a2522d7dad2e5db39de
linaluonto <107812991+linaluonto@users.noreply.github.com>
2022-09-14 17:10:35 -0700

    Upload task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs- computes the absolute value of an integer
+ * @n: Integer n
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}
